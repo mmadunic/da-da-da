2ff154cd771bce81a8fb8f4ad4a24363bbf57f10
Matija Madunić <mmadunic2@pmfst.hr>
2023-04-12 00:44:11 +0200

    Dodana navigacijska traka i footer

---
+++ frontend/rent/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/navbar';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <NavBar/>
+
+
+        <Footer/>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/rent/src/components/navbar.js
@@ -0,0 +1,18 @@
+import './navbar.css';
+import React from 'react';
+
+function NavBar() {
+    return (
+        <div class="topnav">
+            <a class="dis" href="#contact" >+021 345-6789</a>
+            <a class="dis" href="#contact" >rentacar@gmail.com</a>
+            
+            <div class="topnav-right">
+                <a href="#prijava">Prijava</a>
+                <a href="#registracija">Registracija</a>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
