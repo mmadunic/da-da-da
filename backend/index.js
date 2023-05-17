a9ef4e12bcca7358257f239352b59fcd31372c20
Matija Madunić <mmadunic2@pmfst.hr>
2023-05-18 00:21:02 +0200

    Povezano s bazom

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,8 @@
-console.log("Pozzzzzzzz")
+const app = require('./App')  // Express aplikacija
+const http = require('http')
+
+const server = http.createServer(app)
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server je pokrenut na portu ${process.env.PORT}`)
+})
